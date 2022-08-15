dbedd2c313801cef92f77a8cbfaaf63e4aa0af0f
Joao Gosmani <joaogosmani@gmail.com>
2022-08-15 16:08:42 -0300

    Criando a funcionalidade de cadastro de usuário

--- src/main/java/br/com/joaogosmani/ediaristas/web/controllers/UsuarioController.java
+++ src/main/java/br/com/joaogosmani/ediaristas/web/controllers/UsuarioController.java
@@ -1,11 +1,18 @@
 package br.com.joaogosmani.ediaristas.web.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.joaogosmani.ediaristas.web.dtos.FlashMessage;
 import br.com.joaogosmani.ediaristas.web.dtos.UsuarioCadastroForm;
 import br.com.joaogosmani.ediaristas.web.services.WebUsuarioService;
 
@@ -33,5 +40,21 @@ public class UsuarioController {
 
         return modelAndView;
     } 
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(
+        @Valid @ModelAttribute("cadastroForm") UsuarioCadastroForm cadastroForm, 
+        BindingResult result, 
+        RedirectAttributes attrs
+    ) {
+        if (result.hasErrors()) {
+            return "admin/usuario/cadastro-form";
+        }
+
+        service.cadastrar(cadastroForm);
+        attrs.addFlashAttribute("alert", new FlashMessage("alert-succes", "Usuário cadastrado com sucesso!"));
+
+        return "redirect:/admin/usuarios";  
+    }
 
 }
