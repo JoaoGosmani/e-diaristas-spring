34cfe6738493e8576ad371d21a8fec35846f5ccc
Joao Gosmani <joaogosmani@gmail.com>
2022-08-16 14:01:49 -0300

    dto e mapeamento de edição de usuário

--- src/main/java/br/com/joaogosmani/ediaristas/web/mappers/WebUsuarioMapper.java
+++ src/main/java/br/com/joaogosmani/ediaristas/web/mappers/WebUsuarioMapper.java
@@ -5,6 +5,7 @@ import org.mapstruct.factory.Mappers;
 
 import br.com.joaogosmani.ediaristas.core.models.Usuario;
 import br.com.joaogosmani.ediaristas.web.dtos.UsuarioCadastroForm;
+import br.com.joaogosmani.ediaristas.web.dtos.UsuarioEdicaoForm;
 
 @Mapper(componentModel = "spring")
 public interface WebUsuarioMapper {
@@ -12,5 +13,9 @@ public interface WebUsuarioMapper {
    WebUsuarioMapper INSTANCE = Mappers.getMapper(WebUsuarioMapper.class);
    
    Usuario toModel(UsuarioCadastroForm form);
+
+   Usuario toModel(UsuarioEdicaoForm form);
+
+   UsuarioEdicaoForm toForm(Usuario model);
 
 }
