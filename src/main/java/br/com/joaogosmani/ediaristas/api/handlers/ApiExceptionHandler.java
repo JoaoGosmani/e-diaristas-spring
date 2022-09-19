aa7c35bd21c6e35f654487550ad8db7dcbaf1087
Joao Gosmani <joaogosmani@gmail.com>
2022-09-18 23:50:18 -0300

    Criando o método para tratar os erros de validação na API

--- src/main/java/br/com/joaogosmani/ediaristas/api/handlers/ApiExceptionHandler.java
+++ src/main/java/br/com/joaogosmani/ediaristas/api/handlers/ApiExceptionHandler.java
@@ -1,14 +1,23 @@
 package br.com.joaogosmani.ediaristas.api.handlers;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 
 import br.com.joaogosmani.ediaristas.api.dtos.responses.ErrorResponse;
 import br.com.joaogosmani.ediaristas.core.services.consultaendereco.exceptions.EnderecoServiceException;
@@ -16,6 +25,8 @@ import br.com.joaogosmani.ediaristas.core.services.consultaendereco.exceptions.E
 @RestControllerAdvice(annotations = RestController.class)
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     
+    private SnakeCaseStrategy camelCaseToSnakeCase = new SnakeCaseStrategy();
+
     @ExceptionHandler(EnderecoServiceException.class)
     public ResponseEntity<Object> handleEnderecoServiceException(
         EnderecoServiceException exception, HttpServletRequest request
@@ -28,6 +39,32 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
             .build();
 
             return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException exception,
+        HttpHeaders headers, 
+        HttpStatus status, 
+        WebRequest request
+    ) {
+        var body = new HashMap<String, List<String>>();
+
+        exception.getBindingResult().getFieldErrors()
+            .forEach(fieldError -> {
+                var field = camelCaseToSnakeCase.translate(fieldError.getField());
+
+                if (!body.containsKey(field)) {
+                    var fieldErrors = new ArrayList<String>();
+                    fieldErrors.add(fieldError.getDefaultMessage());
+
+                    body.put(field, fieldErrors);
+                } else {
+                    body.get(field).add(fieldError.getDefaultMessage());
+                }
+            });
+
+        return ResponseEntity.badRequest().body(body);
     }
 
 }
