4a8cbb99abd58f96e0e98b492c84c48badfec070
Joao Gosmani <joaogosmani@gmail.com>
2022-09-20 16:21:25 -0300

    Criando a rota de login

---
+++ src/main/java/br/com/joaogosmani/ediaristas/api/controllers/AuthRestController.java
@@ -0,0 +1,27 @@
+package br.com.joaogosmani.ediaristas.api.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.joaogosmani.ediaristas.api.dtos.requests.TokenRequest;
+import br.com.joaogosmani.ediaristas.api.dtos.responses.TokenResponse;
+import br.com.joaogosmani.ediaristas.api.services.ApiAuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthRestController {
+    
+    @Autowired
+    private ApiAuthService service;
+
+    @PostMapping("/token")
+    public TokenResponse autenticar(@RequestBody @Valid TokenRequest tokenRequest) {
+        return service.autenticar(tokenRequest);
+    }
+
+}
