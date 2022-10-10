69039f2bc106d0c6f9c3aec261baeb9fd660841e
Joao Gosmani <joaogosmani@gmail.com>
2022-10-10 12:37:34 -0300

    Criando a action de listagem de diárias

--- src/main/java/br/com/joaogosmani/ediaristas/core/repositories/DiariaRepository.java
+++ src/main/java/br/com/joaogosmani/ediaristas/core/repositories/DiariaRepository.java
@@ -1,9 +1,16 @@
 package br.com.joaogosmani.ediaristas.core.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.joaogosmani.ediaristas.core.models.Diaria;
+import br.com.joaogosmani.ediaristas.core.models.Usuario;
 
 public interface DiariaRepository extends JpaRepository<Diaria, Long> {
     
+    List<Diaria> findByCliente(Usuario cliente);
+
+    List<Diaria> findByDiarista(Usuario diarista);
+
 }
