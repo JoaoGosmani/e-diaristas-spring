286d6543f1002fb77c890973bf0ad71e7c416084
Joao Gosmani <joaogosmani@gmail.com>
2022-10-17 11:56:48 -0300

    Adicionando os links de candidatar-se em uma diária e listagem de oportunidades no HATEOAS

--- src/main/java/br/com/joaogosmani/ediaristas/api/assemblers/UsuarioAssembler.java
+++ src/main/java/br/com/joaogosmani/ediaristas/api/assemblers/UsuarioAssembler.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import br.com.joaogosmani.ediaristas.api.controllers.CidadesAtendidasRestController;
 import br.com.joaogosmani.ediaristas.api.controllers.DiariaRestController;
 import br.com.joaogosmani.ediaristas.api.controllers.EnderecoDiaristaRestController;
+import br.com.joaogosmani.ediaristas.api.controllers.OportunidadeRestController;
 import br.com.joaogosmani.ediaristas.api.dtos.responses.UsuarioResponse;
 
 @Component
@@ -40,7 +41,11 @@ public class UsuarioAssembler implements Assembler<UsuarioResponse> {
                 .withRel("cidades_atendidas")
                 .withType("GET");
 
-            resource.adicionarLinks(atualizarEnderecoLink, listarEnderecoLink, relacionarCidadesLink, cidadesAtendidasLink);
+            var listaOportunidadesLink = linkTo(methodOn(OportunidadeRestController.class).buscarOportunidades())
+                .withRel("lista_oportunidades")
+                .withType("GET");
+
+            resource.adicionarLinks(atualizarEnderecoLink, listarEnderecoLink, relacionarCidadesLink, cidadesAtendidasLink, listaOportunidadesLink);
         }
 
         var listaDiariasLink = linkTo(methodOn(DiariaRestController.class).listarPorUsuarioLogado())
