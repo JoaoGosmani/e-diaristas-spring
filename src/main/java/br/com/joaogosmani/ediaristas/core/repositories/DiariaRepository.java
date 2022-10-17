9bfa129ebbe8f6442c45d0ef0102ae637e50c250
Joao Gosmani <joaogosmani@gmail.com>
2022-10-17 11:16:53 -0300

    Criando a query de busca de oportunidades

--- src/main/java/br/com/joaogosmani/ediaristas/core/repositories/DiariaRepository.java
+++ src/main/java/br/com/joaogosmani/ediaristas/core/repositories/DiariaRepository.java
@@ -3,6 +3,7 @@ package br.com.joaogosmani.ediaristas.core.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.joaogosmani.ediaristas.core.models.Diaria;
 import br.com.joaogosmani.ediaristas.core.models.Usuario;
@@ -12,5 +13,25 @@ public interface DiariaRepository extends JpaRepository<Diaria, Long> {
     List<Diaria> findByCliente(Usuario cliente);
 
     List<Diaria> findByDiarista(Usuario diarista);
+
+    @Query(
+        """
+        SELECT
+            d
+        FROM
+            Diaria d
+        WHERE
+            d.status = br.com.joaogosmani.ediaristas.core.enums.DiariaStatus.PAGO
+        AND
+            d.diarista IS NULL
+        AND
+            d.codigoIbge IN :cidades
+        AND
+            :candidato NOT MEMBER OF d.candidatos
+        AND
+            SIZE(d.candidatos) < 3
+        """
+    )
+    List<Diaria> findOportunidades(List<String> cidades, Usuario cadidato);
 
 }
