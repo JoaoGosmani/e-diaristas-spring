c0b85617ff676d1cb4b4f600f11e736e9e1e4439
Joao Gosmani <joaogosmani@gmail.com>
2022-10-17 10:44:09 -0300

    Validando a data de atendimento da diária

--- src/main/java/br/com/joaogosmani/ediaristas/core/validators/CandidaturaValidator.java
+++ src/main/java/br/com/joaogosmani/ediaristas/core/validators/CandidaturaValidator.java
@@ -1,5 +1,7 @@
 package br.com.joaogosmani.ediaristas.core.validators;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.FieldError;
@@ -60,7 +62,20 @@ public class CandidaturaValidator {
             var mensagem = "Diária não está com o status PAGO";
             var fieldError = new FieldError(diaria.getClass().getName(), "status", null, false, null, null, mensagem);
             throw new ValidacaoException(mensagem, fieldError);
-        }    
+        }   
+        
+        validarDataAtendimentoDiaria(diaria);
+    }
+
+    private void validarDataAtendimentoDiaria(Diaria diaria) {
+        var hoje = LocalDateTime.now();
+        var dataAtendimento = diaria.getDataAtendimento();
+
+        if (hoje.isAfter(dataAtendimento)) {
+            var mensagem = "Diária com data de atendimento no passado";
+            var fieldError = new FieldError(diaria.getClass().getName(), "dataAtendimento", null, false, null, null, mensagem);
+            throw new ValidacaoException(mensagem, fieldError);    
+        }
     }
 
 }
