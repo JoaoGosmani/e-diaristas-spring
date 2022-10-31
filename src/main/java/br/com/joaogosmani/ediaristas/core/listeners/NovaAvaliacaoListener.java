ff9a8eef20b729b20e1e8d02f2eb969007bdd62d
Joao Gosmani <joaogosmani@gmail.com>
2022-10-31 12:14:23 -0300

    Atualizando o status da diária avaliada

--- src/main/java/br/com/joaogosmani/ediaristas/core/listeners/NovaAvaliacaoListener.java
+++ src/main/java/br/com/joaogosmani/ediaristas/core/listeners/NovaAvaliacaoListener.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import br.com.joaogosmani.ediaristas.core.enums.DiariaStatus;
 import br.com.joaogosmani.ediaristas.core.events.NovaAvaliacaoEvent;
 import br.com.joaogosmani.ediaristas.core.models.Avaliacao;
 import br.com.joaogosmani.ediaristas.core.repositories.AvaliacaoRepository;
+import br.com.joaogosmani.ediaristas.core.repositories.DiariaRepository;
 import br.com.joaogosmani.ediaristas.core.repositories.UsuarioRepository;
 
 @Component
@@ -18,10 +20,22 @@ public class NovaAvaliacaoListener {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private DiariaRepository diariaRepository;
+
     @EventListener
     public void handleNovaAvaliacaoEvent(NovaAvaliacaoEvent event) {
         var avaliacao = event.getAvaliacao();
         atualizarReputacaoAvaliado(avaliacao);
+        atualizarStatusDiariaAvaliada(avaliacao);
+    }
+
+    private void atualizarStatusDiariaAvaliada(Avaliacao avaliacao) {
+        var diaria = avaliacao.getDiaria();
+        if (avaliacaoRepository.isClienteAndDiaristaAvaliaramDiaria(diaria)) {
+            diaria.setStatus(DiariaStatus.AVALIADO);
+            diariaRepository.save(diaria);
+        }
     }
 
     private void atualizarReputacaoAvaliado(Avaliacao avaliacao) {
