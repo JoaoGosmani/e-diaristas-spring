bc84d9308bb6efd2b5ae59cb60f052d431470907
Joao Gosmani <joaogosmani@gmail.com>
2022-12-14 22:41:39 -0300

    Criando a funcionalidade de atualização de dados do usuário

--- src/main/java/br/com/joaogosmani/ediaristas/api/services/ApiUsuarioService.java
+++ src/main/java/br/com/joaogosmani/ediaristas/api/services/ApiUsuarioService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.FieldError;
 import org.springframework.web.multipart.MultipartFile;
 
+import br.com.joaogosmani.ediaristas.api.dtos.requests.AtualizarUsuarioRequest;
 import br.com.joaogosmani.ediaristas.api.dtos.requests.UsuarioRequest;
 import br.com.joaogosmani.ediaristas.api.dtos.responses.MensagemResponse;
 import br.com.joaogosmani.ediaristas.api.dtos.responses.TokenResponse;
@@ -13,6 +14,7 @@ import br.com.joaogosmani.ediaristas.api.dtos.responses.UsuarioCadastroResponse;
 import br.com.joaogosmani.ediaristas.api.dtos.responses.UsuarioResponse;
 import br.com.joaogosmani.ediaristas.api.mappers.ApiUsuarioMapper;
 import br.com.joaogosmani.ediaristas.core.exceptions.SenhasNaoConferemException;
+import br.com.joaogosmani.ediaristas.core.models.Usuario;
 import br.com.joaogosmani.ediaristas.core.publishers.NovoUsuarioPublisher;
 import br.com.joaogosmani.ediaristas.core.repositories.UsuarioRepository;
 import br.com.joaogosmani.ediaristas.core.services.storage.adapters.StorageService;
@@ -80,6 +82,39 @@ public class ApiUsuarioService {
         usuarioLogado.setFotoUsuario(foto);
         repository.save(usuarioLogado);
         return new MensagemResponse("Foto salva com sucesso!");
+    }
+    
+    public MensagemResponse atualizar(AtualizarUsuarioRequest request) {
+        var usuarioLogado = securityUtils.getUsuarioLogado();
+
+            atualizarInformacoesUsuarioLogado(request, usuarioLogado);
+
+            validator.validar(usuarioLogado);
+
+            repository.save(usuarioLogado);
+
+            return new MensagemResponse("Usuário atualizado com sucesso!");
+    }
+
+    private void atualizarInformacoesUsuarioLogado(AtualizarUsuarioRequest request, Usuario usuarioLogado) {
+        usuarioLogado.setNomeCompleto(
+            request.getNomeCompleto() != null ? request.getNomeCompleto() : usuarioLogado.getNomeCompleto()
+        );
+            usuarioLogado.setEmail(
+                request.getEmail() != null ? request.getEmail() : usuarioLogado.getEmail()
+            );
+            usuarioLogado.setCpf(
+                request.getCpf() != null ? request.getCpf() : usuarioLogado.getCpf()
+            );
+            usuarioLogado.setNascimento(
+                request.getNascimento() != null ? request.getNascimento() : usuarioLogado.getNascimento()
+            );
+            usuarioLogado.setTelefone(
+                request.getTelefone() != null ? request.getTelefone() : usuarioLogado.getTelefone()
+            );
+            usuarioLogado.setChavePix(
+                request.getChavePix() != null ? request.getChavePix() : usuarioLogado.getChavePix()
+            );
     }
 
     private TokenResponse gerarTokenResponse(UsuarioCadastroResponse response) {
